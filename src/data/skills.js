54cd4d0013086b774290859d8c9b8c026ab7fe58
firas boukhchim <boukhchimfiras487@gmail.com>
2024-01-09 19:41:26 +0100

    4th

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,12 +1,12 @@
 
 const skills = [
     { imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1969px-Laravel.svg.png', name: 'Laravel' },
-    { imageUrl: '/assets/react.png', name: 'React.js' },
+    { imageUrl: './assets/react.png', name: 'React.js' },
     { imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-java-60-1174953.png', name: 'Java' },
     { imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-python-3521655-2945099.png', name: 'Python' },
     { imageUrl: '/assets/spring-boot-logo-removebg-preview.png', name: 'Spring Boot' },
     { imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-postgresql-11-1175122.png', name: 'PostgreSQL' },
-    { imageUrl: '/assets/git.png', name: 'Git' },
+    { imageUrl: './assets/git.png', name: 'Git' },
     { imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-docker-226091.png', name: 'docker' },
     { imageUrl: 'https://miro.medium.com/v2/resize:fit:265/1*mvX3NIp0lA3kS0cdI-0HTg.png', name: 'Jupyter' },
     { imageUrl: 'https://img.icons8.com/color/48/000000/nodejs.png', name: 'Node.js' },
