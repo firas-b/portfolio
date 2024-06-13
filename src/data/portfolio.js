d82bcc323024a35c9e5ceb6757cd49e1e3a626d8
firas boukhchim <66187210+firas-b@users.noreply.github.com>
2024-06-13 12:46:20 +0100

    second commit commit

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,15 +1,20 @@
 export default [
-   {
-      title: 'Blogs App',
-      imgUrl: './assets/blogs.png',
-      stack: ['.net mvc', 'Identity', 'SQLserver'],
-      link: 'https://github.com/firas-b/Blogs-with-Auth',
-   },
-   {
-      title: 'Laravel APP sportner',
-      imgUrl: './assets/sportner.png',
-      stack: ['Laravel', 'Blade', 'MySQL'],
-      link: 'https://github.com/firas-b/Blogs-with-Auth',
-   }
-
-]
+  {
+    title: "E-vents App",
+    imgUrl: "./assets/events.png",
+    stack: [".net ", "react", "jwt", "SQLserver"],
+    link: "https://www.linkedin.com/feed/update/urn:li:activity:7204835052617150464/",
+  },
+  {
+    title: "Blogs App",
+    imgUrl: "./assets/blogs.png",
+    stack: [".net mvc", "Identity", "SQLserver"],
+    link: "https://github.com/firas-b/Blogs-with-Auth",
+  },
+  {
+    title: "Laravel APP sportner",
+    imgUrl: "./assets/sportner.png",
+    stack: ["Laravel", "Blade", "MySQL"],
+    link: "https://github.com/firas-b/Blogs-with-Auth",
+  },
+];
--- src/data/skills.js
+++ src/data/skills.js
@@ -1,18 +1,60 @@
-
 const skills = [
-    { imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1969px-Laravel.svg.png', name: 'Laravel' },
-    { imageUrl: './assets/react.png', name: 'React.js' },
-    { imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-java-60-1174953.png', name: 'Java' },
-    { imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-python-3521655-2945099.png', name: 'Python' },
-    { imageUrl: './assets/spring-boot-logo-removebg-preview.png', name: 'Spring Boot' },
-    { imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-postgresql-11-1175122.png', name: 'PostgreSQL' },
-    { imageUrl: './assets/git.png', name: 'Git' },
-    { imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-docker-226091.png', name: 'docker' },
-    { imageUrl: 'https://miro.medium.com/v2/resize:fit:265/1*mvX3NIp0lA3kS0cdI-0HTg.png', name: 'Jupyter' },
-    { imageUrl: 'https://img.icons8.com/color/48/000000/nodejs.png', name: 'Node.js' },
-    { imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Android_robot.svg/872px-Android_robot.svg.png', name: 'android'},
-    {imageUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/.NET_Core_Logo.svg/1024px-.NET_Core_Logo.svg.png',name:'.NET'}
-    // Add more skills as needed
+  {
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1969px-Laravel.svg.png",
+    name: "Laravel",
+  },
+  { imageUrl: "./assets/react.png", name: "React.js" },
+  {
+    imageUrl:
+      "https://cdn.iconscout.com/icon/free/png-256/free-java-60-1174953.png",
+    name: "Java",
+  },
+  {
+    imageUrl:
+      "https://cdn.iconscout.com/icon/free/png-256/free-python-3521655-2945099.png",
+    name: "Python",
+  },
+  {
+    imageUrl:
+      "https://w7.pngwing.com/pngs/752/651/png-transparent-angularjs-vue-js-others-angle-rectangle-triangle-thumbnail.png",
+    name: "Angular",
+  },
+  {
+    imageUrl: "./assets/spring-boot-logo-removebg-preview.png",
+    name: "Spring Boot",
+  },
+  {
+    imageUrl:
+      "https://cdn.iconscout.com/icon/free/png-256/free-postgresql-11-1175122.png",
+    name: "PostgreSQL",
+  },
+  { imageUrl: "./assets/git.png", name: "Git" },
+  {
+    imageUrl:
+      "https://cdn.iconscout.com/icon/free/png-256/free-docker-226091.png",
+    name: "docker",
+  },
+  {
+    imageUrl:
+      "https://miro.medium.com/v2/resize:fit:265/1*mvX3NIp0lA3kS0cdI-0HTg.png",
+    name: "Jupyter",
+  },
+  {
+    imageUrl: "https://img.icons8.com/color/48/000000/nodejs.png",
+    name: "Node.js",
+  },
+  {
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Android_robot.svg/872px-Android_robot.svg.png",
+    name: "android",
+  },
+  {
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/.NET_Core_Logo.svg/1024px-.NET_Core_Logo.svg.png",
+    name: ".NET",
+  },
+  // Add more skills as needed
 ];
 
 export default skills;
